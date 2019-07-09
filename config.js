083ae41f7468088872d4129b613370324a516541
Shivam0Anand <shivam_anand@outlook.com>
2019-07-09 16:37:45 +0530

    update

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ module.exports = {
   copyright: "© All rights reserved.",
   disqusShortname: "",
   postsPerPage: 4,
-  googleAnalyticsId: "UA-73379983-2",
+  googleAnalyticsId: "UA-143545798-2",
   menu: [
     {
       label: "Articles",
